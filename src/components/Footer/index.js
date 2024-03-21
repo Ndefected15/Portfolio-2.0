9ace73185760ee2c500c61df8cbc104e0982d9cf
Nick D <Ndefected15@gmail.com>
2024-03-21 16:24:06 -0400

    Fixed mobile ui issues

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import './index.scss'
+
+const Footer = () => {
+  return (
+    <footer>
+      <p className="footer-title">Coded by Nicholas Defex</p>
+    </footer>
+  )
+}
+
+export default Footer
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,14 +1,17 @@
 import { Outlet } from 'react-router-dom'
 import Sidebar from '../Sidebar'
+import Footer from '../Footer'
 import './index.scss'
 
 const Layout = () => {
   return (
     <div className="App">
       <Sidebar />
+
       <div className="page">
         <Outlet />
       </div>
+      <Footer />
     </div>
   )
 }
